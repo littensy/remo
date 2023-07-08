850c2e2d063cc5318ae01d5576f5b944a8448a4f
littensy <56808540+littensy@users.noreply.github.com>
2023-07-07 23:43:39 -0700

    feat: use scoped namespaces (#4)
    * feat: prepend remote names with the namespace
    
    * docs: update readme

--- src/server/init.lua
+++ src/server/init.lua
@@ -2,14 +2,15 @@ local types = require(script.Parent.types)
 local createAsyncRemote = require(script.createAsyncRemote)
 local createRemote = require(script.createRemote)
 
-local function createRemotes(builders: types.RemoteBuilders): types.RemoteMap
+local function createRemotes(builders: types.RemoteBuilders, namespace: string?): types.RemoteMap
 	local remotes: types.RemoteMap = {}
+	local scope = if namespace then `{namespace}.` else ""
 
 	for name, builder in builders do
 		remotes[name] = if builder.type == "namespace"
-			then createRemotes(builder.remotes)
-			elseif builder.type == "event" then createRemote(name, builder)
-			elseif builder.type == "function" then createAsyncRemote(name, builder)
+			then createRemotes(builder.remotes, scope .. name)
+			elseif builder.type == "event" then createRemote(scope .. name, builder)
+			elseif builder.type == "function" then createAsyncRemote(scope .. name, builder)
 			else error(`Invalid remote type "{builder.type}"`)
 	end
 
