72f6df060cf3dd8831220d6e516115466c3ca0a4
Richard <richerd.rdc@gmail.com>
2023-07-07 22:17:01 -0700

    feat: add throttle middleware

---
+++ src/middleware/throttleMiddleware.lua
@@ -0,0 +1,148 @@
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+local Promise = require(script.Parent.Parent.Promise)
+local types = require(script.Parent.Parent.types)
+local constants = require(script.Parent.Parent.constants)
+local getSender = require(script.Parent.Parent.getSender)
+
+local SERVER = "(SERVER)"
+
+export type ThrottleMiddlewareOptions = {
+	throttle: number?,
+	trailing: boolean?,
+	[any]: any,
+} | number
+
+local function timeout<T...>(callback: (T...) -> (), amount: number, ...: T...): () -> ()
+	local timer = 0
+	local connection
+
+	connection = RunService.Heartbeat:Connect(function(deltaTime)
+		timer += deltaTime
+		if timer >= amount then
+			connection:Disconnect()
+			callback()
+		end
+	end)
+
+	return function()
+		connection:Disconnect()
+	end
+end
+
+local function throttleMiddleware(options: ThrottleMiddlewareOptions?): types.Middleware
+	local throttle = 0.1
+	local trailing = false
+
+	if type(options) == "number" then
+		throttle = options
+	elseif type(options) == "table" then
+		throttle = if options.throttle ~= nil then options.throttle else throttle
+		trailing = if options.trailing ~= nil then options.trailing else trailing
+	end
+
+	local eventMiddleware: types.Middleware = function(next, remote)
+		local timeouts: { [string]: () -> () } = {}
+		local cache: { [string]: { [number]: unknown, n: number } } = {}
+
+		return function(...)
+			local sender = getSender(...)
+			local senderId = if sender then sender.Name else SERVER
+			local timeoutId = timeouts[senderId]
+
+			cache[senderId] = table.pack(...)
+
+			if timeoutId then
+				if not constants.IS_TEST then
+					warn(`🔴 throttled remote '{remote.name}' fired by '{senderId}'`)
+				end
+				return
+			end
+
+			timeouts[senderId] = timeout(function()
+				local arguments = cache[senderId]
+				timeouts[senderId] = nil
+				cache[senderId] = nil
+
+				if trailing then
+					next(table.unpack(arguments, 1, arguments.n))
+				end
+			end, throttle)
+
+			return next(...)
+		end
+	end
+
+	local asyncMiddleware: types.Middleware = function(next, remote)
+		local timeouts: { [string]: () -> () } = {}
+		local cache: { [string]: { [number]: unknown, n: number } } = {}
+		local pending: { [string]: boolean } = {}
+		local watching: { [string]: Promise.Promise } = {}
+
+		local function clearCacheOnPlayerDisconnect(sender: Player)
+			if watching[sender.Name] then
+				return
+			end
+
+			watching[sender.Name] = Promise.fromEvent(Players.PlayerRemoving, function(player)
+				return player == sender
+			end):andThen(function()
+				timeouts[sender.Name] = nil
+				cache[sender.Name] = nil
+				pending[sender.Name] = nil
+				watching[sender.Name] = nil
+			end)
+		end
+
+		return function(...)
+			local sender = getSender(...)
+			local senderId = if sender then sender.Name else SERVER
+
+			local timeoutId = timeouts[senderId]
+			local isPending = pending[senderId]
+
+			if sender then
+				clearCacheOnPlayerDisconnect(sender)
+			end
+
+			if timeoutId or isPending then
+				-- async remotes should try to return the previous value before
+				-- rejecting the request
+				local results =
+					assert(cache[senderId], `🔴 throttled remote '{remote.name}' requested by '{senderId}'`)
+				return table.unpack(results, 1, results.n)
+			end
+
+			pending[senderId] = true
+
+			local ok, results = pcall(function(...)
+				return table.pack(next(...))
+			end, ...)
+
+			pending[senderId] = nil
+
+			timeouts[senderId] = timeout(function()
+				timeouts[senderId] = nil
+			end, throttle)
+
+			assert(ok, results)
+
+			cache[senderId] = results
+
+			return table.unpack(results, 1, results.n)
+		end
+	end
+
+	return function(next, remote)
+		if remote.type == "event" then
+			return eventMiddleware(next, remote)
+		elseif remote.type == "function" then
+			return asyncMiddleware(next, remote)
+		else
+			return next
+		end
+	end
+end
+
+return throttleMiddleware
---
+++ src/middleware/throttleMiddleware.spec.lua
@@ -0,0 +1,307 @@
+return function()
+	local RunService = game:GetService("RunService")
+
+	local types = require(script.Parent.Parent.types)
+	local createRemotes = require(script.Parent.Parent.createRemotes)
+	local builder = require(script.Parent.Parent.builder)
+	local instances = require(script.Parent.Parent.utils.instances)
+	local throttleMiddleware = require(script.Parent.throttleMiddleware)
+
+	local function pause(frames: number?)
+		local counter = frames or 1
+		local thread = coroutine.running()
+		local connection
+
+		connection = RunService.Heartbeat:Connect(function()
+			counter -= 1
+			if counter <= 0 then
+				connection:Disconnect()
+				task.defer(thread)
+			end
+		end)
+
+		coroutine.yield()
+	end
+
+	describe("event throttle", function()
+		local remotes, remote: types.ServerToClient, instance: RemoteEvent
+
+		local function create(options: throttleMiddleware.ThrottleMiddlewareOptions)
+			remotes = createRemotes({ remote = builder.remote() }, throttleMiddleware(options))
+			remote = remotes.remote
+			instance = instances.createRemoteEvent("remote")
+		end
+
+		afterEach(function()
+			remotes:destroy()
+		end)
+
+		it("should throttle subsequent calls", function()
+			local calls = 0
+
+			create({ throttle = 0, trailing = false })
+
+			remote:connect(function()
+				calls += 1
+			end)
+
+			for _ = 1, 5 do
+				instance:FireServer()
+			end
+			expect(calls).to.equal(1)
+
+			pause()
+
+			for _ = 1, 5 do
+				instance:FireServer()
+			end
+			expect(calls).to.equal(2)
+		end)
+
+		it("should emit trailing calls", function()
+			local calls = 0
+
+			create({ throttle = 0, trailing = true })
+
+			remote:connect(function()
+				calls += 1
+			end)
+
+			for _ = 1, 5 do
+				instance:FireServer()
+			end
+			expect(calls).to.equal(1)
+
+			pause()
+			expect(calls).to.equal(2)
+
+			for _ = 1, 5 do
+				instance:FireServer()
+			end
+			expect(calls).to.equal(3)
+
+			pause()
+			expect(calls).to.equal(4)
+		end)
+
+		it("should pass the latest arguments to the trailing call", function()
+			local calls = 0
+			local arg1, arg2
+
+			create({ throttle = 0, trailing = true })
+
+			remote:connect(function(player, a, b)
+				calls += 1
+				arg1, arg2 = a, b
+			end)
+
+			for i = 1, 5 do
+				instance:FireServer(i, i + 1)
+			end
+			instance:FireServer("a", "b")
+
+			expect(calls).to.equal(1)
+			expect(arg1).to.be.a("number")
+			expect(arg2).to.be.a("number")
+
+			pause()
+			expect(calls).to.equal(2)
+			expect(arg1).to.equal("a")
+			expect(arg2).to.equal("b")
+
+			instance:FireServer("c", "d")
+			expect(calls).to.equal(3)
+			expect(arg1).to.equal("c")
+			expect(arg2).to.equal("d")
+
+			pause()
+			expect(calls).to.equal(4)
+			expect(arg1).to.equal("c")
+			expect(arg2).to.equal("d")
+		end)
+
+		it("should receive a throttle time as options", function()
+			local calls = 0
+
+			create(0)
+
+			remote:connect(function()
+				calls += 1
+			end)
+
+			for _ = 1, 5 do
+				instance:FireServer()
+			end
+			expect(calls).to.equal(1)
+
+			pause()
+
+			for _ = 1, 5 do
+				instance:FireServer()
+			end
+			expect(calls).to.equal(2)
+		end)
+	end)
+
+	describe("async throttle", function()
+		local remotes, remote: types.ServerToClientAsync, instance: RemoteFunction
+
+		local function create(options: throttleMiddleware.ThrottleMiddlewareOptions)
+			remotes = createRemotes({ remote = builder.remote().returns() }, throttleMiddleware(options))
+			remote = remotes.remote
+			instance = instances.createRemoteFunction("remote")
+		end
+
+		local function didYield(callback: () -> any)
+			local blocked = true
+			local success, result
+
+			task.spawn(function()
+				success, result = pcall(callback)
+				blocked = false
+			end)
+
+			assert(success or success == nil, result)
+
+			return blocked
+		end
+
+		afterEach(function()
+			remotes:destroy()
+		end)
+
+		it("should throttle subsequent calls", function()
+			local calls = 0
+
+			create({ throttle = 0 })
+
+			remote:onRequest(function(): ()
+				calls += 1
+			end)
+
+			for _ = 1, 5 do
+				instance:InvokeServer()
+			end
+			expect(calls).to.equal(1)
+
+			pause()
+
+			for _ = 1, 5 do
+				instance:InvokeServer()
+			end
+			expect(calls).to.equal(2)
+		end)
+
+		it("should return the cached value when throttled", function()
+			local acc = 0
+
+			create({ throttle = 0 })
+
+			remote:onRequest(function(): number
+				acc += 1
+				return acc
+			end)
+
+			expect(instance:InvokeServer()).to.equal(1)
+			for _ = 1, 5 do
+				expect(instance:InvokeServer()).to.equal(1)
+			end
+
+			pause()
+
+			expect(instance:InvokeServer()).to.equal(2)
+			for _ = 1, 5 do
+				expect(instance:InvokeServer()).to.equal(2)
+			end
+		end)
+
+		it("should throw if the initial cache is not ready", function()
+			create({ throttle = 0 })
+
+			remote:onRequest(function(): ()
+				pause()
+			end)
+
+			task.spawn(function()
+				instance:InvokeServer()
+			end)
+
+			expect(function()
+				instance:InvokeServer()
+			end).to.throw()
+		end)
+
+		it("should return the cached value if a request is still pending", function()
+			local acc = 0
+
+			create({ throttle = 0 })
+
+			remote:onRequest(function(): number
+				-- set cache synchronously on first request
+				if acc > 0 then
+					pause()
+				end
+				acc += 1
+				return acc
+			end)
+
+			expect(didYield(function(): ()
+				-- initialize cache immediately
+				expect(instance:InvokeServer()).to.equal(1)
+			end)).to.equal(false)
+
+			expect(didYield(function(): ()
+				-- throttle not over, return cached value
+				expect(instance:InvokeServer()).to.equal(1)
+				expect(instance:InvokeServer()).to.equal(1)
+			end)).to.equal(false)
+
+			pause()
+
+			expect(didYield(function(): ()
+				-- make a new request, it will eventually return 2
+				expect(instance:InvokeServer()).to.equal(2)
+			end)).to.equal(true)
+
+			expect(didYield(function(): ()
+				-- throttle not over, return cached value
+				expect(instance:InvokeServer()).to.equal(1)
+				expect(instance:InvokeServer()).to.equal(1)
+			end)).to.equal(false)
+		end)
+
+		it("should throttle if the handler throws an error", function()
+			local acc = 0
+
+			create({ throttle = 0 })
+
+			remote:onRequest(function(): number
+				acc += 1
+				assert(acc ~= 1, "error")
+				return acc
+			end)
+
+			expect(function()
+				instance:InvokeServer() -- handler error
+			end).to.throw()
+
+			expect(function()
+				instance:InvokeServer() -- throttle error
+			end).to.throw()
+
+			pause()
+
+			expect(instance:InvokeServer()).to.equal(2)
+			for _ = 1, 5 do
+				expect(instance:InvokeServer()).to.equal(2)
+			end
+
+			pause()
+
+			expect(instance:InvokeServer()).to.equal(3)
+			for _ = 1, 5 do
+				expect(instance:InvokeServer()).to.equal(3)
+			end
+		end)
+	end)
+end
