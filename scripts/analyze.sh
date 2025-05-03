5b6436787ce22b5bd8a1d78a2de335664768885d
littensy <56808540+littensy@users.noreply.github.com>
2025-05-03 13:27:19 -0700

    chore: housekeeping

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -1,7 +1,7 @@
-curl -o scripts/roblox.d.lua https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua
+curl -o scripts/roblox.d.luau https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua
 
 rojo sourcemap test.project.json -o sourcemap.json
 
-luau-lsp analyze --defs=scripts/testez.d.lua --defs=scripts/roblox.d.lua --base-luaurc=.luaurc --sourcemap=sourcemap.json --ignore="**/_Index/**" --no-strict-dm-types src
+luau-lsp analyze --defs=scripts/testez.d.luau --defs=scripts/roblox.d.luau --sourcemap=sourcemap.json --base-luaurc=.luaurc --ignore="**/_Index/**" src
 
-rm scripts/roblox.d.lua
+rm scripts/roblox.d.luau
